223c63e443d15f034f37e7d683100ab6646b7e99
fo0revery0oung <leylaaars@student.ub.ac.id>
2026-03-28 18:05:26 +0700

    Update MahasiswaController.php

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -7,20 +7,15 @@ use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
-    // Menampilkan halaman + data mahasiswa
     public function index()
     {
         $data = Mahasiswa::all();
         return view('mahasiswa', compact('data'));
     }
 
-    // Menyimpan data mahasiswa dari form
     public function store(Request $request)
     {
-        // Simpan data
         Mahasiswa::create($request->all());
-
-        // Redirect kembali ke halaman utama
         return redirect('/');
     }
 }
